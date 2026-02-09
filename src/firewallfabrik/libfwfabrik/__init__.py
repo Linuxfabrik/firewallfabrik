04ae9969fed3fc74f836e4331210c9aabd07c5d1
Navid Sassan <navid.sassan@linuxfabrik.ch>
2026-02-09 21:27:42 +0100

    style: remove duplicate License headers

--- src/firewallfabrik/libfwfabrik/__init__.py
+++ src/firewallfabrik/libfwfabrik/__init__.py
@@ -10,18 +10,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-# Copyright (C) 2026 Linuxfabrik <info@linuxfabrik.ch>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# On Debian systems, the complete text of the GNU General Public License
-# version 2 can be found in /usr/share/common-licenses/GPL-2.
-
-# SPDX-License-Identifier: GPL-2.0-or-later
-
 from ._database import DatabaseManager
 from ._xml_reader import (
     ParseResult,
